9697b42733a30a1da983f44835a59585c0e5a555
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2025-12-07 00:14:38 +0500

    have to change filename - error: enum Weekday is public, should be declared in a file named Weekday.java public enum Weekday {

--- app/src/main/java/com/nightonke/githubwidget/WeekDay.java
+++ app/src/main/java/com/nightonke/githubwidget/WeekDay.java
@@ -3,8 +3,12 @@ package com.nightonke.githubwidget;
 /**
  * Created by Weiping on 2016/4/26.
  */
-public enum Weekday {
+//public enum Weekday {
+// D:\programDo\GithubWidget\app\src\main\java\com\nightonke\githubwidget\WeekDay.java:6: error: enum Weekday is public, should be declared in a file named Weekday.java
+// public enum Weekday {
+// Chanded filename to Weekday
 
+public enum Weekday {
     SUN(0),
     MON(1),
     TUE(2),

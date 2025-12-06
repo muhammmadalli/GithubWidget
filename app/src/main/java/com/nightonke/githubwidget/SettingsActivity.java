5b961ac3b8ebd5a548a24d41c77513140fe88049
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2025-12-07 02:23:13 +0500

    Android app compatibility support package update
    changed android.support.v7.app.AppCompatActivity to x.appcompat.app.AppCompatActivity in **Activity.java files

--- app/src/main/java/com/nightonke/githubwidget/SettingsActivity.java
+++ app/src/main/java/com/nightonke/githubwidget/SettingsActivity.java
@@ -6,7 +6,7 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
